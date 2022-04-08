632cde81e4c80ee7ec8f84185fc8e5579b7a6927
amr303 <amr.abdulmatin@gmail.com>
2022-04-08 17:11:54 +0600

    Display logged in user info, Sign out, toggle sign in, sign out

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import './App.css';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import app from './firebase.init';
-import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+import { useState } from 'react';
 
 const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({});
   const provider = new GoogleAuthProvider();
+
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
-    .then(result => {
-      const user = result.user;
-      console.log(user);
-    })
-    .catch(error => {
-      console.log('error', error);
-    })
+      .then(result => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch(error => {
+        console.log('error', error)
+      })
+
+  }
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch(error => {
+        setUser({});
+      })
 
   }
   return (
     <div className="App">
-     <button onClick={handleGoogleSignIn}>Google sign In</button>
+      {
+        user.email ? <button onClick={handleSignOut}>Sign Out</button>
+          :
+          <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      }
+      <h2>{user.displayName}</h2>
+      <p>I know your email address: {user.email}</p>
+      <img src={user.photoURL} alt="" />
+
     </div>
   );
 }
