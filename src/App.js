424d18dd4b64a84d171d08b329788b53824f0b84
amr303 <amr.abdulmatin@gmail.com>
2022-04-08 14:07:09 +0600

    open google login popup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 import app from './firebase.init';
+import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
 
+const auth = getAuth(app);
 
 function App() {
+  const provider = new GoogleAuthProvider();
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+    .then(result => {
+      const user = result.user;
+      console.log(user);
+    })
+    .catch(error => {
+      console.log('error', error);
+    })
+
+  }
   return (
     <div className="App">
-     
+     <button onClick={handleGoogleSignIn}>Google sign In</button>
     </div>
   );
 }
--- src/_steps.js
+++ src/_steps.js
@@ -6,5 +6,8 @@
 * 4. Copy firebase init with config from firebase project settings into a file    firbase.init.js
 * 5. export default app
 * 6. import app firebase init.js into your app.js
-* 7.
+* 7. import getAuth from firebase/auth and create auth = getAuth(app)
+* 8. turn on google authentication (firebase > authentication > enable google sign in)
+* 9. create google provider
+* 10. 
 */
